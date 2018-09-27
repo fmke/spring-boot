7c1705eb45a6ea0520855dafdbf3bda9e9c9fa35
mke <michael.ke@rentacenter.com>
2018-09-27 15:14:13 -0500

    update

--- citest/src/test/java/com/citest/CitestApplicationTests.java
+++ citest/src/test/java/com/citest/CitestApplicationTests.java
@@ -2,15 +2,27 @@ package com.citest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.citest.endpoint.MsgController;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CitestApplicationTests {
 
+	@Autowired
+	private MsgController msgController;
+	
+	
 	@Test
 	public void contextLoads() {
+		
+		 assertThat(msgController).isNotNull();
 	}
 
 }
