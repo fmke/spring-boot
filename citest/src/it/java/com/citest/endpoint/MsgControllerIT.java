0e6d597494cf273052c762ab4429927a30d3f04a
mke <michael.ke@rentacenter.com>
2018-09-27 15:36:48 -0500

    update

---
+++ citest/src/it/java/com/citest/endpoint/MsgControllerIT.java
@@ -0,0 +1,19 @@
+package com.citest.endpoint;
+
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MsgControllerIT {
+
+	RestTemplate restTemplate = new RestTemplate();
+	
+	@Test
+	public void testSendMsg() {
+		ResponseEntity<String> response
+		  = restTemplate.getForEntity("http://localhost:8082/msg?msg=testSendMsg", String.class);
+		
+		assertThat(response.getBody()).isEqualTo("testSendMsg");
+	}
+}
