7a841771044114751ca447007f86ed7c647ccf6d
mmit <test@example.com>
2018-11-17 14:04:33 -0600

    update

--- citest/src/main/java/com/citest/endpoint/MsgController.java
+++ citest/src/main/java/com/citest/endpoint/MsgController.java
@@ -1,5 +1,8 @@
 package com.citest.endpoint;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,8 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class MsgController {
 	
 	@RequestMapping(value="/msg", method=RequestMethod.GET)
-	public String sendMsg(@RequestParam(name="msg") String msg) {
+	public String sendMsg(@RequestParam(name="msg") String msg,
+			HttpServletRequest request, 
+	        HttpServletResponse response) {
+		
+		System.out.println(request.getRequestURI());
 		
 		return msg;
+	}
+	
+	@RequestMapping(value="/authCb", method=RequestMethod.GET)
+	public void authCb(@RequestParam(name="authCb") String msg,
+			HttpServletRequest request, 
+	        HttpServletResponse response) {
+		
+		System.out.println(request.getRequestURI());
+		System.out.println(request.getQueryString());
 	}
 }
